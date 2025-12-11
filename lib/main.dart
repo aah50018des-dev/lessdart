66fe504ade0c8fd452a0d879d29a86c92da96320
Abdulraheem Altamimi <aah50018des@gmail.com>
2025-12-11 16:54:39 +0300

    lesson4

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(SettingsPage());
+  runApp(MyLifecycleApp());
 }
-class SettingsPage extends StatefulWidget {
+
+class MyLifecycleApp extends StatefulWidget {
   @override
-  _SettingsPageState createState() => _SettingsPageState();
+  _MyLifecycleAppState createState() => _MyLifecycleAppState();
 }
 
-class _SettingsPageState extends State<SettingsPage> {
-  TextEditingController _usernameController = TextEditingController();
-  bool _darkModeEnabled = false;
-
+class _MyLifecycleAppState extends State<MyLifecycleApp> {
   @override
   void initState() {
     super.initState();
-    _loadSettings();
+    print("initState called");
   }
 
-  // Method to load saved settings
-  Future<void> _loadSettings() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _usernameController.text = prefs.getString('username') ?? '';
-      _darkModeEnabled = prefs.getBool('darkMode') ?? false;
-    });
-  }
-
-  // Method to save settings
-  Future<void> _saveSettings() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('username', _usernameController.text);
-    await prefs.setBool('darkMode', _darkModeEnabled);
-    print('Settings saved');
-  }
-
-  // Method to Delete settings
-  Future<void> _clearSettings() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove('username'); // Remove username
-    await prefs.remove('darkMode'); // Remove dark mode preference
+  @override
+  void dispose() {
+    print("dispose called");
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    print("build called");
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Settings')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'Username'),
-              ),
-              SwitchListTile(
-                title: Text('Dark Mode'),
-                value: _darkModeEnabled,
-                onChanged: (bool value) {
-                  setState(() {
-                    _darkModeEnabled = value;
-                  });
-                },
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: _saveSettings,
-                    child: Text('Save Settings'),
-                  ),
-                  SizedBox(width: 20,),
-                  ElevatedButton(
-                    onPressed: _clearSettings,
-                    child: Text('clear setting'),
-                  ),
-                ],
-              ),
-
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('Stateful Widget Lifecycle'),
+        ),
+        body: Center(
+          child: Text('Check the console for lifecycle methods.'),
         ),
       ),
     );
